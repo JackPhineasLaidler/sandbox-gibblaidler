8c3084d87b0b35b23d7fb25f1a8a47f2fa144d19
Jack Laidler <jacklaidler@hotmail.com>
2024-01-23 12:29:59 +0000

    contact-form-sitekey

--- pages/contact.js
+++ pages/contact.js
@@ -186,7 +186,7 @@ const Contact = () => {
                             </div>
                         </div>
                         <ReCAPTCHA
-                            sitekey={process.env.NEXT_PUBLIC_SITE_KEY}
+                            sitekey={process.env.APP_SECRET_KEY}
                             size="invisible"
                             ref={captchaRef}
                         />
